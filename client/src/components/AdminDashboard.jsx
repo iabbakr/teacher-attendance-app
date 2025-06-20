d10f36195e8df5b5324e9f3d17a1f9b0af9e67e7
iabbakr <abubakaribrahim603@gmail.com>
2025-06-20 11:29:10 +0100

    added changes

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -91,7 +91,7 @@ function AdminDashboard() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className=" min-h-screen bg-gray-100">
       <nav className="bg-blue-600 p-4 text-white flex justify-between">
         <div>
           <button
